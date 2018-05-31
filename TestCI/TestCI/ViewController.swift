559f5f27d7163a0ae5d9151e3419789c8702d410
Vinay Chopra <vinay.chopra@olacabs.com>
2018-05-31 17:09:59 +0530

    adding xcode proj

---
+++ TestCI/TestCI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestCI
+//
+//  Created by Vinay Chopra on 31/05/18.
+//  Copyright © 2018 Vinay Chopra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
